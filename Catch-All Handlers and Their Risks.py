0f51d173c328f6f1e845c4930734d57a48716136
Harshada Chavan09 <harshachavan1103@gmail.com>
2026-09-07 12:44:56 +0530

    Add files via upload

---
+++ Catch-All Handlers and Their Risks.py
@@ -0,0 +1,8 @@
+try:
+    res = "100" / 20 
+    
+except ArithmeticError:
+    print("Arithmetic problem.")
+    
+except:
+    print("Something went wrong!")
---
+++ basic example of exception.py
@@ -0,0 +1,5 @@
+n=10
+try:
+    res = n/0
+except ZeroDivisionError:
+    print("can't be divided by zero!")
